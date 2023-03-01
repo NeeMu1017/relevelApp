fc256519e0698265c7912fba8ed5ff8ecce4a11d
NirmalPatel <nirs2435@outlook.com>
2023-03-01 17:01:11 +0530

    start

--- crmapp/crmapp/src/componts/ticket/Ticket.js
+++ crmapp/crmapp/src/componts/ticket/Ticket.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import "./Ticket.css"
 
 import { Navbar } from '../Navbar/Navbar';
 import { useNavigate } from 'react-router';
 
 const Ticket = () => {
     const [tickets, setTickets] = useState([]);
+    const [fillterTicket,setFillterTicket] = useState([]);
     const naviagte = useNavigate();
 
     useEffect(() => {
@@ -12,6 +14,7 @@ const Ticket = () => {
             .then((res) => res.json())
             .then((parsedRes) => {
                 setTickets(parsedRes);
+                setFillterTicket(parsedRes)
             });
     }, []);
 
@@ -19,20 +22,38 @@ const Ticket = () => {
         naviagte("/ticketform/" + desc);
     }
 
-
+    function handleSearch(key){
+        const result = tickets.filter(t => t.desc.includes(key));
+        
+        setFillterTicket(result);
+    }
     return (
         <div>
-           <Navbar/>
-           <div>
-                <a
-                    href="/ticketform"
-                    className="btn btn-success mb-3">
-                    New Ticket
-                </a>
-           </div>
+            <Navbar />
+            <div className='container m-3'>
+                <div className='container' >
+                    <div className='col-10'>
+                        <button
+                            onClick={() => { naviagte("/ticketform") }}
+                            className="btn btn-success mb-3">
+                            New Ticket
+                        </button>
+                    </div>
+                    <div className="search-box-wrapper">
+                        <input
+                            placeholder="Search..."
+                            onInput={(e) =>handleSearch(e.target.value)}
+                            className="search-box" type="search" />&nbsp;&nbsp;
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                        </svg>
+                    </div>
+                </div>
+
+            </div>
             <div className="container">
 
-               
+
                 <table className="table">
                     <thead>
                         <tr>
@@ -46,7 +67,7 @@ const Ticket = () => {
                     </thead>
                     <tbody>
                         {
-                            tickets.map(t =>
+                            fillterTicket.map(t =>
                                 <tr>
                                     <td>{t.customer}</td>
                                     <td>{t.desc}</td>
